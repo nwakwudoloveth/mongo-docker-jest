3cd4af9ec29b85d6ddf370d5ddc887c7d2328081
Loveth Nwakwudo <nwakwudoloveth@gmail.com>
2022-12-11 20:50:14 +0000

    refactored HTTP requests

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,47 +5,69 @@ const User = require('../models/user')
 const Product = require('../models/product')
 
 router.get('/initialise', async function (req, res) {
-  console.log('---------------------------')
   const shoe = new Product({ image: 'url', price: 1500, description: 'This is a good shoe' })
   shoe.save()
   const mariana = new User({ name: 'mariana', email: 'mariana@gmail.com', password: 'password' })
   const loveth = new User({ name: 'Loveth', email: 'loveth@gmail.com', password: 'password' })
-
   const zeynep = new User({ name: 'Zeynep', email: 'Zeynep@gmail.com', password: 'password' })
-
   zeynep.list(shoe)
   loveth.list(shoe)
   mariana.list(shoe)
-  await mariana.save()
-  await loveth.save()
-  await zeynep.save()
-  // const users = [mariana, loveth, zeynep]
-  console.log(zeynep)
-  console.log(loveth)
-  console.log(mariana)
   res.sendStatus(200)
-  // res.send(users)
 })
-
-// router.get('/', async function (req, res) {
-//   const result = await User.find({})
-
-//   // if (req.query.name) {
-//   //   result = U.filter(user => user.name === req.query.name)
-//   // }
-//   res.send(result)
-// })
-router.get('/', async function (req, res) {
-  const users = await User.find({})
-  if (users) res.render('user', { users })
-  else res.sendStatus(404)
-  // res.send(users)
+// get users Request
+router.get('/', async function (req, res, next) {
+  try {
+    const users = await User.find({})
+    res.render('user', { users })
+  } catch (e) {
+    res.sendStatus(404)
+  }
 })
-
-router.post('/', async function (req, res) {
+// post request
+router.post('/', async function (req, res, next) {
   const { name, email, password } = req.body
-
-  const user = await User.create({ name, email, password })
-  res.send(user)
+  try {
+    if (!email || !name || !password) {
+      res
+        .send({
+          message: 'Missing fields.',
+        })
+        .status(400)
+      return
+    }
+    const user = await User.create({
+      name,
+      email,
+      password,
+    })
+    res.send(user)
+  } catch (e) {
+    next(e)
+  }
 })
+
+// update a user
+router.patch('/:id', async function (req, res, next) {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, {
+      name: req.body.name,
+      email: req.body.email,
+    })
+    res.send(user)
+  } catch (e) {
+    next(e)
+  }
+})
+
+// delete a user
+router.delete('/:id', async function (req, res, next) {
+  try {
+    await User.findByIdAndDelete(req.params.id)
+    res.sendStatus(200)
+  } catch (e) {
+    next(e)
+  }
+})
+
 module.exports = router
